b69e1bb89996f08953ecd524e3b75b8b9f8e2ad4
leonardo valle <leonardo.valle@eduzz.com>
2022-08-10 09:25:34 -0300

    exercicio de callbacks

---
+++ programacaoFuncional/funcoes/callbacks_2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
+const caminho = path.join(__dirname, "dados.txt");
+
+function exibirConteudo(_, conteudo) {
+  console.log(conteudo.toString());
+}
+
+fs.readFile(caminho, exibirConteudo);
+fs.readFileSync(caminho, exibirConteudo);
